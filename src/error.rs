5552a84d904fdfc74363bc9b2922870b3f2a026d
Robert Schütte <roba@Roberts-MBP.fritz.box>
2017-11-17 09:04:35 +0100

    continued refactoring
    Removed depreacted rust_serialize\nRemoved all unwrap() functions

--- src/error.rs
+++ src/error.rs
@@ -7,10 +7,10 @@ error_chain!{
     foreign_links {
         Fmt(::std::fmt::Error);
         Io(::std::io::Error);
-        FormBase64(::rustc_serialize::base64::FromBase64Error);
-        //CipherError(::crypto::symmetriccipher::SymmetricCipherError);
         Utf8(::std::str::Utf8Error);
         FromUtf8(::std::string::FromUtf8Error);
         Reqwest(::reqwest::Error);
+        Regex(::regex::Error);
+        Base64(::base64::DecodeError);
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,18 @@
 #[macro_use] extern crate error_chain;
 extern crate reqwest;
-extern crate rustc_serialize;
 extern crate crypto;
 extern crate regex;
+extern crate base64;
+
+pub mod error;
 
 use std::io::Read;
 use std::ops::Deref;
 use std::str;
-
-pub mod error;
-
 use error::*;
 use reqwest::Client;
 use reqwest::header::{Connection, UserAgent};
 use regex::Regex;
-use rustc_serialize::base64::FromBase64;
-
 use crypto::{buffer, aes, blockmodes };
 use crypto::buffer::{ ReadBuffer, WriteBuffer };
 
@@ -53,111 +50,14 @@ impl PkgData {
     }
 }
 
-fn get_key(key: &[u8], app_name: &str) -> Result<Vec<u8>> {
-    let url = "http://service.jdownloader.org/dlcrypt/service.php?srcType=dlc&destType=".to_string()
-        + app_name + "&data=" + try!(str::from_utf8(key));
-
-    let client = Client::new();
-    let mut res = client.get(&url)
-        .header(Connection::close())
-        .header(UserAgent::new("Mozilla/5.3 (Windows; U; Windows NT 5.1; de; rv:1.8.1.6) Gecko/2232 Firefox/3.0.0.R"))
-        .send()?;
-
-    let mut key = Vec::new();
-    res.read_to_end(&mut key)?;
-
-    if key.len() != 33 {
-        bail!("Unexpected Error");
-    };
-    // remove <rc> and </rc>
-    let key = key[4..28].to_vec();
-    let key = try!(key.from_base64());
-
-    return Ok(key);
-}
-
-fn aes_decrypt(data: Vec<u8>, dec_key: &[u8], dec_iv: &[u8]) -> Result<Vec<u8>> {
-    let mut out = [0; 4096];
-    let mut reader = buffer::RefReadBuffer::new(data.deref());
-    let mut writer = buffer::RefWriteBuffer::new(&mut out);
-    let mut dec = aes::cbc_decryptor(
-        aes::KeySize::KeySize128,
-        dec_key,
-        dec_iv,
-        blockmodes::NoPadding,
-    );
-
-    let mut result = Vec::new();
-    loop {
-        // clean this up and use error_chain
-        dec.decrypt(&mut reader, &mut writer, true).unwrap();
-        if writer.is_empty() {
-            break;
-        }
-        result.extend_from_slice(writer.take_read_buffer().take_remaining());
-    };
-
-    // remove tailing zeros
-    result.retain(|x| *x !=  0 as u8);
-
-    return Ok(result);
-}
-
-fn decrypt_raw(data: Vec<u8>, app_name: &str, dec_key: &[u8], dec_iv: &[u8]) -> Result<Vec<u8>> {
-    let len = data.len();
-    if len <= 88 {
-        bail!("Corrupted data");
-    };
-    let (data, key) = data.split_at(len-88);
-
-    let key = try!(get_key(key, app_name));
-    let key = try!(aes_decrypt(key, dec_key, dec_iv));
-
-    let data = try!(data.from_base64());
-    let data = try!(aes_decrypt(data, key.deref(), key.deref()));
-    let data = try!(data.from_base64());
-
-    return Ok(data);
-}
-
-fn pkg_details(data: String) -> Result<(String, String)> {
-    let re = Regex::new(r#"name="([^"])*""#).unwrap();
-    let (s, e) = re.find(&data).unwrap();
-    let name = data[s+6..e-1].to_string();
-
-    let re = Regex::new(r#"passwords="([^"])*""#).unwrap();
-    let (s, e) = re.find(&data).unwrap();
-    let pwd = data[s+11..e-1].to_string();
-
-    let name = try!(name.from_base64());
-    let name = try!(String::from_utf8(name));
-    let pwd = try!(pwd.from_base64());
-    let pwd = try!(String::from_utf8(pwd));
-
-    Ok((name, pwd))
-}
-
-fn file_details(data: String, pos: usize) -> String {
-    let buf: String = match data[pos..].from_base64() {
-        Ok(x)  => {
-            match String::from_utf8(x) {
-                Ok(x)  => x,
-                Err(_) => data[pos..].to_string(),
-            }
-        },
-        Err(_) => data[pos..].to_string(),
-    };
-    buf
-}
 
 pub fn decrypt_dlc(data: Vec<u8>, app_name: &str, dec_key: &[u8], dec_iv: &[u8]) -> Result<PkgData> {
-    let data = try!(decrypt_raw(data, app_name, dec_key, dec_iv));
-    let data = try!(String::from_utf8(data));
-    // let data = data.trim();
+    let data = decrypt_raw(data, app_name, dec_key, dec_iv)?;
+    let data = String::from_utf8(data)?;
     let mut pkg = PkgData::new();
 
-    let re = Regex::new(r#"<package ([^>]*)"#).unwrap();
-    let (s, e) = re.find(&data).unwrap();
+    let re = Regex::new(r#"<package ([^>]*)"#)?;
+    let (s, e) = re.find(&data).ok_or("Can't find package in data")?;
     let (name, pwd) = match pkg_details(data[s..e].to_string()) {
         Ok((name, pwd)) => (name, pwd),
         Err(_)          => (String::new(), String::new()),
@@ -185,4 +85,97 @@ pub fn decrypt_dlc(data: Vec<u8>, app_name: &str, dec_key: &[u8], dec_iv: &[u8])
     };
 
     return Ok(pkg);
+}
+
+/************ Private Functions *****************/
+
+fn get_key(key: &[u8], app_name: &str) -> Result<Vec<u8>> {
+    let url = "http://service.jdownloader.org/dlcrypt/service.php?srcType=dlc&destType=".to_string()
+        + app_name + "&data=" + try!(str::from_utf8(key));
+
+    let client = Client::new();
+    let mut res = client.get(&url)
+        .header(Connection::close())
+        .header(UserAgent::new("Mozilla/5.3 (Windows; U; Windows NT 5.1; de; rv:1.8.1.6) Gecko/2232 Firefox/3.0.0.R"))
+        .send()?;
+
+    let mut key = Vec::new();
+    res.read_to_end(&mut key)?;
+
+    if key.len() != 33 {
+        bail!("Unexpected Error");
+    };
+    // remove <rc> and </rc>
+    let key = base64::decode(&key[4..28])?;
+
+    Ok(key)
+}
+
+fn aes_decrypt(data: Vec<u8>, dec_key: &[u8], dec_iv: &[u8]) -> Result<Vec<u8>> {
+    let mut out = [0; 4096];
+    let mut reader = buffer::RefReadBuffer::new(data.deref());
+    let mut writer = buffer::RefWriteBuffer::new(&mut out);
+    let mut dec = aes::cbc_decryptor(
+        aes::KeySize::KeySize128,
+        dec_key,
+        dec_iv,
+        blockmodes::NoPadding,
+    );
+
+    let mut result = Vec::new();
+    loop {
+        if dec.decrypt(&mut reader, &mut writer, true).is_err() {
+            bail!("Can't decrypt");
+        }
+        if writer.is_empty() {
+            break;
+        }
+        result.extend_from_slice(writer.take_read_buffer().take_remaining());
+    };
+
+    // remove tailing zeros
+    result.retain(|x| *x !=  0 as u8);
+
+    return Ok(result);
+}
+
+fn decrypt_raw(data: Vec<u8>, app_name: &str, dec_key: &[u8], dec_iv: &[u8]) -> Result<Vec<u8>> {
+    let len = data.len();
+    if len <= 88 {
+        bail!("Corrupted data");
+    };
+    let (data, key) = data.split_at(len-88);
+
+    let key = aes_decrypt(get_key(key, app_name)?, dec_key, dec_iv)?;
+
+    let data = base64::decode(data)?;
+    let data = aes_decrypt(data, key.deref(), key.deref())?;
+    let data = base64::decode(&data)?;
+
+    return Ok(data);
+}
+
+fn pkg_details(data: String) -> Result<(String, String)> {
+    let re = Regex::new(r#"name="([^"])*""#)?;
+    let (s, e) = re.find(&data).ok_or("Can't find name in data")?;
+    let name = String::from_utf8(base64::decode(&data[s+6..e-1])?)?;
+
+    let re = Regex::new(r#"passwords="([^"])*""#)?;
+    let (s, e) = re.find(&data).ok_or("Can't find password in data")?;
+    let pwd = String::from_utf8(base64::decode(&data[s+11..e-1])?)?;
+
+    Ok((name, pwd))
+}
+
+fn file_details(data: String, pos: usize) -> String {
+    let buf: String = match base64::decode(&data[pos..]) {
+        Ok(x)  => {
+            match String::from_utf8(x) {
+                Ok(x)  => x,
+                Err(_) => data[pos..].to_string(),
+            }
+        },
+        Err(_) => data[pos..].to_string(),
+    };
+    buf
 }
